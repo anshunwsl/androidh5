d4dff57e75ef0f41d8ca4d8c88e88fad949815ea
agisv2 <agisv2@sina.cn>
2019-04-09 23:42:37 +0800

    add  h5 index.html file

--- app/src/main/java/agisvx/org/app/MainActivity.java
+++ app/src/main/java/agisvx/org/app/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    //
+    private WebView webView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //
+        this.webView = (WebView) findViewById(R.id.webView);
+        //
+        //
+        WebSettings webSettings = this.webView.getSettings();
+        //
+
+        webSettings.setJavaScriptEnabled(true);
+        //
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        //
+        //
+//        String fileUrl="https://www.baidu.com";
+        String fileUrl="file:///android_asset/index.html";
+        //
+
+        this.webView.loadUrl(fileUrl);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +48,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    //
+
+    //重载onKeyDown的函数，使其在页面内回退,而不是直接退出程序
+    //
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //
+        if(keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()){
+            this.webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
